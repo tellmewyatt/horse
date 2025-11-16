3ceec8c9420be1aca724f6bdd6a0d368c7373249
Wyatt Cannon <composer@wyattcannon.com>
2025-11-16 18:32:27 -0500

    updated movie splitter

--- moviesplitter.py
+++ moviesplitter.py
@@ -1,36 +1,39 @@
 from moviepy import VideoFileClip, TextClip, CompositeVideoClip, concatenate_videoclips, clips_array
+from moviepy.video.fx.Loop import Loop
 import os
 import random
+from argparse import ArgumentParser
 
-dir = "/media/wyatt/X9 Pro/horse_videos/to_use"
-aspect_ratio = [16,9]
-videos = []
-for filename in os.listdir(dir):
-    if filename.endswith(".mp4") and not filename.startswith("."):
-        videos.append(VideoFileClip(os.path.join(dir, filename)).resized((640,480)))
 
-def random_subclip(clips, max_dur = 20):
+def get_change_points(total_dur, min_dur=10, max_dur=40):
+    durs = []
+    while sum(durs) < total_dur:
+        durs.append(random.randrange(min_dur, max_dur))
+    return durs
+def fill_dur_with_random_subclip(clips, total_dur, min_dur=10):
     clip = random.choice(clips)
-    if clip.duration < max_dur:
-        return clip.subclipped(random.random() * clip.duration, clip.duration)
+    if clip.duration < min_dur:
+        return clip.with_effects([Loop(duration=total_dur)])
     else:
-        inskip = random.random() * clip.duration;
-        return clip.subclipped(inskip, min(inskip + random.random() * max_dur, clip.duration))
-
-def get_random_sequence(clips, duration):
-    current_dur = 0
-    subclips = []
-    while current_dur < duration:
-        clip = random_subclip(clips)
-        current_dur = current_dur + clip.duration;
-        subclips.append(clip)
-    return concatenate_videoclips(subclips)
-def get_row(videos, duration, columns=2):
-    return [get_random_sequence(videos, 60 * 7) for i in range(0, columns)]
-dur = 20
-n = 2
-final_array = [get_row(videos, dur, n) for x in range(0, n)]
-print(final_array)
-final = clips_array(final_array)
-final = final.resized((640, 480))
-final.write_videofile("test.mp4")
+        start = random.random() * (clip.duration - min_dur)
+        return clip.subclipped(start, clip.duration).with_effects([Loop(duration=total_dur)])
+def get_sequence(videos, change_points):
+    return concatenate_videoclips([fill_dur_with_random_subclip(videos, dur) for dur in change_points])
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("video_dir")
+    parser.add_argument("duration", type=int)
+    parser.add_argument("--width", type=int, default=1920)
+    parser.add_argument("--height", type=int, default=1080)
+    parser.add_argument("--outfile", type=str, default="horse_video.mp4")
+    args = parser.parse_args()
+    videos = []
+    for filename in os.listdir(args.video_dir):
+        if filename.endswith(".mp4") and not filename.startswith("."):
+            videos.append(VideoFileClip(os.path.join(args.video_dir, filename)).resized((args.width / 2, args.height / 2)))
+    change_points = get_change_points(args.duration)
+    n = 2
+    # Create a square matrix
+    final_array = [[get_sequence(videos, change_points) for x in range(0, n)] for x in range(0, n)]
+    final = clips_array(final_array).without_audio()
+    final.write_videofile(args.outfile)
