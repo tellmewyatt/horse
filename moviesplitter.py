fd7de57e2f043a632dd7ae48d06e0b2d703ad881
Wyatt Cannon <composer@wyattcannon.com>
2025-12-03 19:31:31 -0500

    Added condition that same video cannot be played att same time

--- moviesplitter.py
+++ moviesplitter.py
@@ -5,23 +5,35 @@ import random
 from argparse import ArgumentParser
 import math
 
-
+available_videos = {}
 def get_change_points(total_dur, min_dur=10, max_dur=40):
     durs = []
     while sum(durs) < total_dur:
         durs.append(random.randrange(min_dur, max_dur))
     return durs
-def fill_dur_with_random_subclip(clips, total_dur, min_dur=10):
-    clip = random.choice(clips)
+def fill_dur_with_random_subclip(clips, total_dur, i, min_dur=10):
+    """ Fills a duration with random subclips. i is the location in available videos """
+    global available_videos
+    clip = None
+    if available_videos.get(i) == None:
+        clip = random.choice(clips)
+        available_videos[i] = [clip.filename]
+    else:
+        clip = random.choice(clips)
+        while clip.filename in available_videos[i]:
+            print("in while loop", i)
+            clip = random.choice(clips)
+        available_videos[i].append(clip.filename)
     if clip.duration < min_dur:
         return clip.with_effects([Loop(duration=total_dur)])
     else:
         start = random.random() * (clip.duration - min_dur)
         return clip.subclipped(start, clip.duration).with_effects([Loop(duration=total_dur)])
 def get_sequence(videos, change_points):
-    return concatenate_videoclips([
-        fill_dur_with_random_subclip(videos, dur) for dur in change_points
-    ])
+    result = [
+        fill_dur_with_random_subclip(videos, dur, i) for i, dur in enumerate(change_points)
+    ]
+    return concatenate_videoclips(result)
 def crop_video(clip, width=1920/2, height=1080/2):
     """ Crops and centers a clip given a width and height """
     aspect_ratio = width / height
@@ -42,6 +54,7 @@ if __name__ == "__main__":
     parser.add_argument("--width", type=int, default=1920)
     parser.add_argument("--height", type=int, default=1080)
     parser.add_argument("--outfile", type=str, default="horse_video.mp4")
+    parser.add_argument("--fps", type=int, default=30)
     args = parser.parse_args()
     videos = []
     for filename in os.listdir(args.video_dir):
@@ -56,4 +69,4 @@ if __name__ == "__main__":
     # Create a square matrix
     final_array = [[get_sequence(videos, change_points) for x in range(0, n)] for x in range(0, n)]
     final = clips_array(final_array).without_audio()
-    final.write_videofile(args.outfile)
+    final.write_videofile(args.outfile, fps=args.fps, preset='ultrafast')
